3d9ca63aab621c6499bbb8b2360735f6689ae3b9
valeskamdev <valeskkitty@gmail.com>
2022-11-24 20:57:34 -0300

    desafio usando construtores

--- src/classe/DataTeste.java
+++ src/classe/DataTeste.java
@@ -5,15 +5,7 @@ public class DataTeste {
 
         Data d1, d2;
         d1 = new Data();
-        d2 = new Data();
-
-        d1.dia = 2;
-        d1.mes = 5;
-        d1.ano = 2003;
-
-        d2.dia = 15;
-        d2.mes = 4;
-        d2.ano = 2003;
+        d2 = new Data(15, 4, 2003);
 
         System.out.println(d1.obterDataFormatada());
         System.out.println(d2.obterDataFormatada());
