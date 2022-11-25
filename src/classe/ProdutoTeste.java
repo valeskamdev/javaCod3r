1a1e69a997b049444ba8ecc493dca727cfc4251b
valeskamdev <valeskkitty@gmail.com>
2022-11-24 23:58:51 -0300

    desafio usando static

--- src/classe/ProdutoTeste.java
+++ src/classe/ProdutoTeste.java
@@ -4,18 +4,19 @@ public class ProdutoTeste {
     public static void main(String[] args) {
 
         Produto p1, p2;
-        p1 = new Produto("Notebook", 4356.89, 0.25);
+        p1 = new Produto("Notebook", 4356.89);
         p2 = new Produto();
 
         p2.nome = "Caneta Preta";
         p2.preco = 12.56;
-        p2.desconto = 0.29;
 
-        System.out.println(p1.nome);
-        System.out.println(p2.nome);
+        Produto.desconto = 0.50;  // alterando o valor do desconto, acessando diretamente da classe
+
+        System.out.println(p1.nome + " " + p1.precoComDesconto());
+        System.out.println(p2.nome + " " + p2.precoComDesconto());
 
         double precoFinal1 = p1.precoComDesconto();
-        double precoFinal2 = p2.precoComDesconto(0.3);
+        double precoFinal2 = p2.precoComDesconto(0.1);
         double mediaCarrinho = (precoFinal1 + precoFinal2) / 2;
 
         System.out.printf("Média do carrinho: R$%.2f", mediaCarrinho);
