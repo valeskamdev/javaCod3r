30a9bf9f1f6a10f945a2ecd88fcaf9af94deb3a6
valeskamdev <valeskkitty@gmail.com>
2022-11-25 16:07:42 -0300

    Usando a palavra reservada this

--- src/classe/Data.java
+++ src/classe/Data.java
@@ -7,18 +7,19 @@ public class Data {
     int ano;
 
     Data() {
-        dia = 2;
-        mes = 5;
-        ano = 2003;
+//      dia = 2;
+//      mes = 5;
+//      ano = 2003;
+        this(2, 5, 2003);  // this como método, chamando outro construtor (USADO SOMENTE NO CONSTRUTOR)
     }
 
-    Data(int diaInicial, int mesInicial, int anoInicial) {
-        dia = diaInicial;
-        mes = mesInicial;
-        ano = anoInicial;
+    Data(int dia, int mes, int ano) {
+        this.dia = dia;
+        this.mes = mes;
+        this.ano = ano;
     }
 
     String obterDataFormatada() {
-        return String.format("%d/%d/%d", dia, mes, ano);
+        return String.format("%d/%d/%d", this.dia, mes, ano);
     }
 }
