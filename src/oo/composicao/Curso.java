821069982c5b865797f1c4be757c0414b80992cd
valeskamdev <valeskkitty@gmail.com>
2022-11-29 11:33:14 -0300

    Relacionamento muitos para muitos

--- src/oo/composicao/Curso.java
+++ src/oo/composicao/Curso.java
@@ -4,15 +4,15 @@ import java.util.ArrayList;
 
 public class Curso {
 
-    final String nome;
-    final ArrayList<Aluno> alunos = new ArrayList<Aluno>();
+     String nome;
+     ArrayList<Aluno> alunos = new ArrayList<Aluno>();
 
-    public Curso(String nome) {
-        this.nome = nome;
-    }
+     public Curso(String nome) {
+         this.nome = nome;
+     }
 
-    void addAluno(Aluno aluno) {
-        this.alunos.add(aluno);
-        aluno.cursos.add(this);
-    }
+     void addAluno(Aluno aluno) {
+         this.alunos.add(aluno);
+         aluno.cursos.add(this);
+     }
 }
