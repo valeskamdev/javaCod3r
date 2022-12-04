744349e6ca80cd5b95ee4e670bcad382b09bc7a3
valeskamdev <valeskkitty@gmail.com>
2022-12-03 22:50:08 -0300

    usando interface funcional definida pelo Java

---
+++ src/lambdas/CalculoTeste3.java
@@ -0,0 +1,22 @@
+package lambdas;
+
+import java.util.function.BinaryOperator;
+
+public class CalculoTeste3 {
+    public static void main(String[] args) {
+
+        BinaryOperator<Double> calc = (a, b) -> { return a + b; };
+        System.out.println(calc.apply(2.0, 3.0));
+
+        calc = (a, b) -> a * b;
+        System.out.println(calc.apply(2.0,3.0));
+
+        System.out.println();
+
+        BinaryOperator<Integer> calc2 = (a, b) -> { return a + b; };
+        System.out.println(calc2.apply(2, 3));
+
+        calc2 = (a, b) -> a * b;
+        System.out.println(calc2.apply(2,3));
+    }
+}
